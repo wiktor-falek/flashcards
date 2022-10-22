9c1b44e5571bd6f3e0b1269890b8bc062f9596eb
wiktor-falek <falekwiktor@gmail.com>
2022-10-23 01:49:37 +0200

    Tweak and add content to landing page.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,13 +4,10 @@
   <main>
     <h1>Learn effectively with Flashcards</h1>
     <p class="subheading">
-      Improve your study routine and retain informations with ease
+      Improve your study routine and retain information with ease
     </p>
 
-
-    <button>
-      <span> Get Started </span>
-    </button>
+    <a class="button" href="#">Get Started</a>
 
     <h1>Why use flashcards?</h1>
     <p>
@@ -18,34 +15,75 @@
       recall, which helps with retaining study material.
     </p>
 
-    <p>The goal of this app is to simplify the process...</p>
+    <p>
+      The goal of this app is to simplify the process of
+      <a href="https://simple.wikipedia.org/wiki/Spaced_repetition"
+        >spaced repetition</a
+      >.
+    </p>
 
     <h1>How this works?</h1>
 
     <p>
       After learning a concept create a flashcard with a question at the front
-      and an answer at the back, you also have an option to include a code snippet.
+      and an answer at the back, you also have an option to include a code
+      snippet.
     </p>
 
     <p>
-      When you come back later to revise your flashcards, try to answer the question.
-      If you're not 100% sure you know the answer, look at the answer. 
-      Ideally you should repeat this process at least three times.
-      If you feel confident about the topic, mark the flashcard as memorized.
+      When a flashcard shows up during your learning session, try to answer the
+      question. Check if it matches the answer. Ideally you should aim to repeat
+      this process at least three times.
     </p>
 
     <p>
-      If you fully grasped the concept already, mark it as memorized. It won't
-      be presented to you anymore, but it still will be available in Completed
-      Collection.
+      If you feel confident about the topic, mark the flashcard as memorized. It
+      won't be presented to you anymore, but it still will be available in
+      Completed Collection.
     </p>
+
+    <h1>Main Features</h1>
+    <ul>
+      <li>
+        <p>Code snippets with highlighting.</p>
+      </li>
+      <li>
+        <p>Sorting by your custom tags.</p>
+      </li>
+      <li>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </li>
+    </ul>
+
+    <h1>Open source</h1>
+    <p class="subheading">This project is fully open source</p>
+
+    <p>Feel free to contribute to the project or maybe even self host it.</p>
+
+    <p>Click here to visit the Github repository.</p>
+
+    <a class="button" href="https://github.com/wiktor-falek/flashcards"
+      >Github</a
+    >
   </main>
+
+  <footer>Wiktor Falek - All rights reserved</footer>
 </template>
 
 <style scoped>
-button {
+main {
+  padding-bottom: 300px;
+}
+
+button,
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-decoration: none;
+  padding: 10px;
   height: 55px;
-  min-width: 220px;
+  width: 220px;
   border: 2px solid #d33f64;
   color: #d33f64;
   background: transparent;
@@ -55,12 +93,14 @@ button {
   font-weight: bold;
 }
 
-button:hover {
+button:hover,
+.button:hover {
   cursor: pointer;
   transform: scale(1.04);
 }
 
-button:active {
+button:active,
+.button:active {
   transform: scale(1);
 }
 </style>
