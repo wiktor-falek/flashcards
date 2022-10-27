17a3241968f1dd5986fda22048157bad7546843b
wiktor-falek <falekwiktor@gmail.com>
2022-10-27 03:06:12 +0200

    Render cards in ActiveCollection.

--- frontend/src/components/cards/Card.vue
+++ frontend/src/components/cards/Card.vue
@@ -1,7 +1,30 @@
-<script setup></script>
+<script setup>
+import { defineProps, ref } from "vue";
+// const props = defineProps(["front, back, code, reviewedCount, id"]);
+const props = defineProps(["flashcard"]);
+
+
+const isFrontSide = ref(true);
+</script>
 
 <template>
-  <div class="card">Card</div>
+  <div class="card">
+    <h1>Front</h1>
+    <p>{{flashcard.front}}</p>
+  </div>
+  
+  <div class="card" v-if="!isFrontSide">
+    <h1>Back</h1>
+    <p>{{flashcard.back}}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  width: 100%;
+  max-width: 250px;
+  aspect-ratio: 6/9;
+  border: 2px solid orange;
+  background-color: rgb(40, 40, 40);
+}
+</style>
--- frontend/src/components/collections/ActiveCollection.vue
+++ frontend/src/components/collections/ActiveCollection.vue
@@ -1,10 +1,21 @@
 <script setup>
-import Card from '../cards/Card.vue';
-
+import Card from "../cards/Card.vue";
+import { useFlashcardStore } from "../../stores/flashcardStore";
+const flashcardStore = useFlashcardStore();
 </script>
 
 <template>
   <div class="collection collection--active">
-    <Card />
+    <Card
+      v-for="flashcard in flashcardStore.flashcards"
+      :flashcard="flashcard"
+      :key="flashcard._id"
+    />
   </div>
 </template>
+
+<style scoped>
+.collection {
+  display: grid;
+}
+</style>
