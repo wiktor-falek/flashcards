042aa49067ba66638389ef51a751439c56392d3c
wiktor-falek <falekwiktor@gmail.com>
2022-10-27 03:25:28 +0200

    Card bottom section layout

--- frontend/src/components/cards/Card.vue
+++ frontend/src/components/cards/Card.vue
@@ -3,21 +3,34 @@ import { defineProps, ref } from "vue";
 // const props = defineProps(["front, back, code, reviewedCount, id"]);
 const props = defineProps(["flashcard"]);
 
-
 const isFrontSide = ref(true);
 </script>
 
 <template>
   <div class="card" v-if="isFrontSide">
     <h1>Front</h1>
-    <p>{{flashcard.front}}</p>
-    <button class="card__button--flip" @click="isFrontSide = false">Flip</button>
+    <p>{{ flashcard.front }}</p>
+
+    <div class="card__bottom">
+      <button class="card__button--flip" @click="isFrontSide = false">
+        Flip
+      </button>
+      <p>Reviewed: {{ flashcard.reviewedCount }}</p>
+      <button class="card__button--edit" @click="">Edit</button>
+    </div>
   </div>
 
   <div class="card" v-if="!isFrontSide">
     <h1>Back</h1>
-    <p>{{flashcard.back}}</p>
-    <button class="card__button--flip" @click="isFrontSide = true">Flip</button>
+    <p>{{ flashcard.back }}</p>
+
+    <div class="card__bottom">
+      <button class="card__button--flip" @click="isFrontSide = true">
+        Flip
+      </button>
+      <p>Reviewed: {{ flashcard.reviewedCount }}</p>
+      <button class="card__button--edit" @click="">Edit</button>
+    </div>
   </div>
 </template>
 
@@ -34,9 +47,18 @@ const isFrontSide = ref(true);
   margin: 0px auto;
 }
 
-.card__button--flip {
+.card__button--flip,
+.card__button--edit {
   width: 48px;
   height: 48px;
   font-size: 20px;
+}
+
+.card__bottom {
+  margin-top: auto;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
 }
 </style>
