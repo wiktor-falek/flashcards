1fa52d8d695c6faca2d242a8108c2d549d35f50e
wiktor-falek <falekwiktor@gmail.com>
2022-10-31 15:08:51 +0100

    Implement random card generator.

--- frontend/src/stores/flashcardStore.js
+++ frontend/src/stores/flashcardStore.js
@@ -4,13 +4,37 @@ import { ref } from "vue";
 export const useFlashcardStore = defineStore("flashcards", () => {
   const flashcards = ref([]);
 
+  // sorted flashcards ready to be displayed in practise mode
+  const practiseRotation = ref([]);
+
   const findById = (id) => {
-    const flashcard = flashcards.value.find((flashcard) => flashcard._id === id) || null;
+    const flashcard =
+      flashcards.value.find((flashcard) => flashcard._id === id) || null;
     return flashcard;
-  }
+  };
+
+  const _setPractiseRotation = () => {
+    const shuffleArray = (array) => {
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    };
+    practiseRotation.value = shuffleArray(flashcards.value);
+  };
+
+  const getNextCard = () => {
+    if (practiseRotation.value.length === 0) {
+      _setPractiseRotation();
+    }
+    return practiseRotation.value.pop();
+  };
 
   return {
     flashcards,
     findById,
+    getNextCard,
   };
 });
