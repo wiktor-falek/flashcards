408b20ae67d6c4948812a21a332b362bb0cbaf03
wiktor-falek <falekwiktor@gmail.com>
2022-10-31 14:53:35 +0100

    Remove unused import.

--- frontend/src/stores/flashcardStore.js
+++ frontend/src/stores/flashcardStore.js
@@ -1,10 +1,16 @@
 import { defineStore } from "pinia";
-import { computed, ref } from "vue";
+import { ref } from "vue";
 
 export const useFlashcardStore = defineStore("flashcards", () => {
   const flashcards = ref([]);
 
+  const findById = (id) => {
+    const flashcard = flashcards.value.find((flashcard) => flashcard._id === id) || null;
+    return flashcard;
+  }
+
   return {
     flashcards,
+    findById,
   };
 });
