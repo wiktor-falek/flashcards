02edc00b7ba48eb8c6d8e6d37ddc607392705140
wiktor-falek <falekwiktor@gmail.com>
2022-10-31 13:14:23 +0100

    Remove defineEmit imports.

--- frontend/src/components/modals/EditCardModal.vue
+++ frontend/src/components/modals/EditCardModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineEmits, ref } from "vue";
+import { ref } from "vue";
 import { updateFlashcard } from "../../api/flashcardApi.js";
 import { useFlashcardStore } from "../../stores/flashcardStore";
 
