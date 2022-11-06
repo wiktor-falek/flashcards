7475d2aca3b3b787db5fe7ce7d7de87a37a4cbeb
wiktor-falek <falekwiktor@gmail.com>
2022-11-06 11:52:47 +0100

    Remove unused function.

--- frontend/src/stores/flashcardStore.js
+++ frontend/src/stores/flashcardStore.js
@@ -1,13 +1,6 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-const shuffle = (array) => {
-  return array
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-};
-
 export const useFlashcardStore = defineStore("flashcards", () => {
   const flashcards = ref([]);
 
