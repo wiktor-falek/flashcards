3a41f828a5188d0255c681d2b9afc07099911f96
wiktor-falek <falekwiktor@gmail.com>
2022-11-06 20:27:27 +0100

    Production ready (probably) v1.

--- frontend/src/api/flashcardApi.js
+++ frontend/src/api/flashcardApi.js
@@ -1,16 +1,16 @@
 import { get, post, _delete, patch } from "./fetchWrapper";
 
 export const getFlashcards = () => {
-  return get("http://localhost:3000/api/v1/flashcard");
+  return get("https://flashcards-backend.onrender.com/api/v1/flashcard");
 };
 
 export const createNewFlashcard = (front, back, code) => {
   const body = { front, back, code };
-  return post("http://localhost:3000/api/v1/flashcard", body);
+  return post("https://flashcards-backend.onrender.com/api/v1/flashcard", body);
 };
 
 export const deleteFlashcard = (id) => {
-  return _delete(`http://localhost:3000/api/v1/flashcard/${id}`);
+  return _delete(`https://flashcards-backend.onrender.com/api/v1/flashcard/${id}`);
 };
 
 export const updateFlashcard = (id, front = null, back = null, code = null, reviewedCount = null, tags = null) => {
@@ -27,17 +27,17 @@ export const updateFlashcard = (id, front = null, back = null, code = null, revi
     (key) => [undefined, null].includes(fields[key]) && delete fields[key]
   );
 
-  return patch(`http://localhost:3000/api/v1/flashcard/${id}`, fields);
+  return patch(`https://flashcards-backend.onrender.com/api/v1/flashcard/${id}`, fields);
 };
 
 export const increment = (id) => {
-  return post(`http://localhost:3000/api/v1/flashcard/increment/${id}`);
+  return post(`https://flashcards-backend.onrender.com/api/v1/flashcard/increment/${id}`);
 };
 
 export const moveFlashcardToMemorizedCollection = (id) => {
-  return post(`http://localhost:3000/api/v1/flashcard/move/memorized/${id}`);
+  return post(`https://flashcards-backend.onrender.com/api/v1/flashcard/move/memorized/${id}`);
 };
 
 export const moveFlashcardToActiveCollection = (id) => {
-  return post(`http://localhost:3000/api/v1/flashcard/move/flashcards/${id}`);
+  return post(`https://flashcards-backend.onrender.com/api/v1/flashcard/move/flashcards/${id}`);
 };
