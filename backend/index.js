dbf5dfa201778f2d77c663e7b9407a71311e7d04
wiktor-falek <falekwiktor@gmail.com>
2022-11-06 20:25:35 +0100

    Production ready (probably) v1.

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 // MIDDLEWARE
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://flashcards-frontend.onrender.com/",
     optionsSuccessStatus: 200,
     credentials: true,
   })
