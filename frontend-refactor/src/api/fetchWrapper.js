1eb4d39a657c79f55d1561c7aa29072366abac4c
wiktor-falek <falekwiktor@gmail.com>
2022-11-25 00:22:41 +0100

    Add jsdoc

--- frontend-refactor/src/api/fetchWrapper.js
+++ frontend-refactor/src/api/fetchWrapper.js
@@ -3,7 +3,6 @@
  * @param {string} method 
  * @param {string} url 
  * @param {string | null} body 
- * @returns fetch(url, { url, body } )
  */
 function request(method, url, body = null) {
   const options = {
@@ -19,18 +18,37 @@ function request(method, url, body = null) {
   return fetch(url, options);
 }
 
+/**
+ * 
+ * @param {string} url 
+ */
 export function get(url) {
   return request("GET", url);
 }
 
+/**
+ * 
+ * @param {string} url 
+ * @param {object} body 
+ */
 export function post(url, body) {
   return request("POST", url, body);
 }
 
+/**
+ * 
+ * @param {string} url 
+ * @param {object} body 
+ */
 export function patch(url, body) {
   return request("PATCH", url, body);
 }
 
+/**
+ * 
+ * @param {string} url 
+ * @param {object} body 
+ */
 export function _delete(url, body) {
   return request("DELETE", url, body);
 }
