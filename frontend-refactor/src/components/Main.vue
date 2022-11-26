fbc26191fa0176f03aabc373b71df2f6a6500a47
wiktor-falek <falekwiktor@gmail.com>
2022-11-26 02:19:26 +0100

    Start working on Main component.

--- frontend-refactor/src/components/Main.vue
+++ frontend-refactor/src/components/Main.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { onBeforeMount } from "vue";
+import Logo from "../components/Logo.vue";
+// import CreateCardModal from "./modals/CreateCardModal.vue";
+// import authAndLoadFlashcards from "../helpers/authAndLoadFlashcards";
+// import { useFlashcardStore } from "../stores/flashcardStore";
+// import logout from "../helpers/logout";
+
+// const flashcardStore = useFlashcardStore();
+
+// onBeforeMount(() => {
+//   if (!flashcardStore.hasFetchedFlashcards) {
+//     authAndLoadFlashcards();
+//   }
+// });
+</script>
+
 <template>
-  Logged in
+  <header>
+    <Logo />
+    <button @click="logout()">Logout</button>
+  </header>
+
+  <main>
+    <div class="menu">
+      <!-- <CreateCardModal /> -->
+      <router-link to="/browse" class="button">Browse Cards</router-link>
+      <router-link to="/practise" class="button">Practise</router-link>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.menu {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
