61cbfd181979d84921125e307fb89994362cd2f6
wiktor-falek <falekwiktor@gmail.com>
2022-11-29 23:03:52 +0100

    A mimir.

--- frontend-refactor/src/components/Main.vue
+++ frontend-refactor/src/components/Main.vue
@@ -16,17 +16,55 @@ onBeforeMount(() => {
   <main>
     <div class="menu">
       <!-- <CreateCardModal /> -->
-      <router-link to="/browse" class="button">Browse Cards</router-link>
-      <router-link to="/practise" class="button">Practise</router-link>
+      <router-link id="browse" to="/browse">Browse<br />Cards</router-link>
+      <a href="" id="stats">View Stats</a>
+      <a href="" id="add">Add new<br />Card</a>
+      <router-link id="practise" to="/practise">Practise</router-link>
     </div>
   </main>
 </template>
 
 <style scoped>
-.menu {
+main {
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+}
+.menu {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 20px;
+  align-items: center;
+  justify-content: center;
   flex-direction: column;
+  width: 800px;
+}
+
+.menu > a {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  word-wrap: break-word;
+  background-color: rgb(54, 54, 54, 0.6);
+  color: white;
+  font-size: 1.4rem;
+  text-decoration: none;
+  height: 160px;
+}
+
+#browse {
+}
+
+#stats {
+}
+
+#add {
+}
+
+#practise {
+  grid-column: span 3;
 }
 </style>
