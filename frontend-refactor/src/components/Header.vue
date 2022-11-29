3efa9c97393c884a6816a8ebfb43d575468218b0
wiktor-falek <falekwiktor@gmail.com>
2022-11-29 20:20:33 +0100

    Add simple header component

---
+++ frontend-refactor/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Logo from "./Logo.vue";
+import logout from "../helpers/logout";
+</script>
+
+<template>
+  <header>
+    <Logo class="logo-transparent" style="transform: scale(0.5)" />
+    <button @click="logout()">Logout</button>
+  </header>
+</template>
+
+<style scoped>
+</style>
